dfad779a9e179792b15dc0d7993bb4c2f9d43263
Gerson Azabache <gerson_dunk@hotmail.com>
2021-09-30 18:21:37 -0500

    connection string setted and first entity is being defined

---
+++ DAL/Entities/CompanyEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAL.Entities
+{
+    public class CompanyEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string ZipCode { get; set; }
+        public string BankAccount { get; set; }
+        public int OwnerId { get; set; }
+        public int CompanyTypeId { get; set; }
+    }
+}
