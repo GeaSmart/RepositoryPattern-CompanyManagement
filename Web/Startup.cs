bb64884f570d3b2accf47fdc9b43311eff689361
Gerson Azabache <gerson_dunk@hotmail.com>
2021-10-01 11:55:56 -0500

    context configured in startup

--- Web/Startup.cs
+++ Web/Startup.cs
@@ -1,6 +1,8 @@
+using DAL;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -24,6 +26,10 @@ namespace Web
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<ApplicationDBContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("default"))
+            );
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
