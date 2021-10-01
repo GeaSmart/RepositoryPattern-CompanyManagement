3662d30f30552ecd4636902c432d4671024a0391
Gerson Azabache <gerson_dunk@hotmail.com>
2021-10-01 16:22:26 -0500

    trademark entity repository added

---
+++ DAL/Repository/ITrademarkRepository.cs
@@ -0,0 +1,14 @@
+﻿using DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Repository
+{
+    public interface ITrademarkRepository : IRepositoryBase<TrademarkEntity>
+    {
+        Task<IEnumerable<TrademarkEntity>> GetAllAsync();
+        Task<TrademarkEntity> GetAsync(int id);
+    }
+}
---
+++ DAL/Repository/TrademarkRepository.cs
@@ -0,0 +1,25 @@
+﻿using DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Repository
+{
+    public class TrademarkRepository : RepositoryBase<TrademarkEntity>, ITrademarkRepository
+    {
+        public TrademarkRepository(ApplicationDBContext context) : base(context)
+        {
+
+        }
+        public async Task<IEnumerable<TrademarkEntity>> GetAllAsync()
+        {
+            return await FindAllAsync();
+        }
+
+        public async Task<TrademarkEntity> GetAsync(int id)
+        {
+            return await FindAsync(id);
+        }
+    }
+}
