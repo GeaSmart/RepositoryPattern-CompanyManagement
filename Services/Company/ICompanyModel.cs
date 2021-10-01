a5f76e195389f4331398c7879e7a30a0eaba5486
Gerson Azabache <gerson_dunk@hotmail.com>
2021-10-01 18:33:17 -0500

    adding models

---
+++ Services/Company/ICompanyModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Services.Company
+{
+    public interface ICompanyModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string ZipCode { get; set; }
+        public string BankAccount { get; set; }
+
+        //FK
+        public int OwnerId { get; set; }
+        public int CompanyTypeId { get; set; }
+
+        //Navigation properties
+        //public List<TrademarkEntity> Trademarks { get; set; }
+        //public OwnerEntity Owner { get; set; }
+        //public CompanyTypeEntity CompanyType { get; set; }
+    }
+}
